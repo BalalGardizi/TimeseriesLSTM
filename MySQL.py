9377a5fee4e40fef4784730672a8d16d8acd5678
BalalGardizi <39780721+BalalGardizi@users.noreply.github.com>
2020-02-03 15:09:20 +0100

    Update MySQL.py

--- MySQL.py
+++ MySQL.py
@@ -5,7 +5,7 @@ import numpy as np
 # this file is specific to the laptop of Abraham.
 # If you want to run the main.py you have to get a local database like MySQL. You can partly re-use this code
 def dataSQL():
-    SQLALCHEMY_DATABASE_URI = create_engine('mysql+pymysql://root:x^g7FU%y1IeMWa$cGw*O@localhost/pureballast')
+    SQLALCHEMY_DATABASE_URI = create_engine('mysql+pymysql://root:XXXXXXXX*O@localhost/pureballast')
     database = pd.read_sql_query("""SELECT * FROM shipData""", SQLALCHEMY_DATABASE_URI)
     database = database.replace('', np.nan)
     database['log_time'] = pd.to_datetime(database['log_time'])
